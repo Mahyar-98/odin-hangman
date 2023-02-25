e588cc2f36a3557058365297bc9c93af2cacd996
Mahyar <mahyar.erfanian1998@gmail.com>
2023-02-24 19:50:13 -0500

    Add more functionality to the Dictionary class and add display class

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # This class loads an array of words that the computer guesses a word from when the game starts
-class DictionaryLoader
+class Dictionary
   DICTIONARY_FILE = 'google-10000-english-no-swears.txt'
-  attr_reader :words, :dictionary
+  attr_reader :words, :computer_word
   def initialize
     @words = []
     File.foreach(DICTIONARY_FILE) do |word|
@@ -11,4 +11,46 @@ class DictionaryLoader
       @words << word
     end
   end
+
+  def computer_word
+    words[rand(words.length)]
+  end
+
+  def char_in_word?(char)
+    computer_word.include?(char)
+  end
 end
+
+class Display
+  attr_accessor :secret_word
+  def initialize(num_char)
+    @num_char = num_char
+    @secret_word = '_' * num_char
+  end
+
+  def update_secret_word(word, char)
+    for i in 0..word.length
+      secret_word[i] = char if word[i] == char
+    end
+  end
+
+  def incorrect_guess(char)
+    puts "Sorry! The letter '#{char}' does not exist in the word."
+  end
+end
+
+class Player
+  def initialize
+  end
+end
+
+class Game
+  def initialize
+    @player = Player.new
+    @dictionary = Dictionary.new
+    @computer_word = @dictionary.computer_word
+  end
+end
+
+new_game = Dictionary.new
+p new_game.computer_word
